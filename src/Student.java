add1c82c4ea5c61f79f469ed902bf491c1989bb4
nbudhwani <nbudhwa@uwo.ca>
2017-02-14 13:22:12 -0500

    Committing my changes (slide 43)

--- src/Student.java
+++ src/Student.java
@@ -3,6 +3,11 @@
  */
 public class Student extends Person {
     String major;
+    List<Course> courseList;
+
+    public void addCourse(Course c){
+        courseList.add(c);
+    }
 
     public Student(String name, String emailAddress, String major) {
         super(name, emailAddress);
